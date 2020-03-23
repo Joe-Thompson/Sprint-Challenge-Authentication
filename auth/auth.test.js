cb06475b057a90d4b47db4ede8ab87ccbea51981
Joe <joe@pop-os.localdomain>
2020-03-22 20:40:49 -0400

    completed tests for auth routes

---
+++ auth/auth.test.js
@@ -0,0 +1,33 @@
+const supertest = require('supertest');
+const server = require('../api/server');
+
+test('login route', async () => {
+  const res = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'joe', password: '123'});
+    expect(res.statusCode).toBe(200);
+    expect(res.type).toBe('application/json');
+    expect(res.body.message).toMatch(/dad/i)
+});
+
+test('login route no password', async () => {
+    const res = await supertest(server)
+        .post('/api/auth/login')
+        .send({ username: 'Bob'});
+    expect(res.statusCode).toBe(401);
+    expect(res.body.errorMessage).toMatch(/invalid/i);
+});
+
+test('register route', async () => {
+   const res = await supertest(server)
+       .post('/api/auth/register')
+       .send({username: 'Bob', password: '147'});
+   expect(res.statusCode).toBe(201);
+});
+
+test('register route, no password', async () => {
+    const res = await supertest(server)
+        .post('/api/auth/register')
+        .send({username: 'smith'});
+    expect(res.statusCode).toBe(500)
+});
